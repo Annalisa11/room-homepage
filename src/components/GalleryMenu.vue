bde662894866502934e81ea5840d697592902fbe
Annalisa Comin <annalisa.comin@hotmail.com>
2024-09-24 04:44:48 +0200

    improved scss structure + BEM class names

--- src/components/GalleryMenu.vue
+++ src/components/GalleryMenu.vue
@@ -48,25 +48,25 @@ console.log('navOpen: ', navOpen)
   <div class="gallery">
     <header>
       <!-- Mobile Menu -->
-      <div id="sidemenu" class="show-on-mobile">
+      <div id="mobile-menu" class="show-on-mobile">
         <button
-          class="sidemenu__btn"
+          class="nav__menu-btn"
           v-on:click="navOpen = !navOpen"
-          v-bind:class="{ active: navOpen }"
+          v-bind:class="{ 'nav__menu-btn--active': navOpen }"
         >
           <transition name="scale">
-            <HamburgerIcon class="hamburger" v-show="!navOpen" />
+            <HamburgerIcon class="nav__icon nav__icon--hamburger" v-show="!navOpen" />
           </transition>
           <transition name="scale">
-            <CloseIcon class="close" v-show="navOpen" />
+            <CloseIcon class="nav__icon nav__icon--close" v-show="navOpen" />
           </transition>
         </button>
         <transition name="translateX">
           <nav v-show="navOpen" class="nav">
-            <div class="sidemenu__wrapper">
-              <ul class="sidemenu__items">
-                <li class="sidemenu__item" v-for="(navItem, index) in navItems" :key="index">
-                  <a href="#" @click="navigateToSlide(index)">
+            <div class="nav__wrapper">
+              <ul class="nav__items">
+                <li class="nav__item" v-for="(navItem, index) in navItems" :key="index">
+                  <a href="#" @click="navigateToSlide(index)" class="nav__link">
                     {{ navItem }}
                   </a>
                 </li>
@@ -77,20 +77,22 @@ console.log('navOpen: ', navOpen)
       </div>
 
       <!-- Desktop Menu -->
-      <div class="show-on-desktop">
-        <a href="#" class="logo">room</a>
-        <nav>
+      <div id="desktop-menu" class="show-on-desktop">
+        <a href="#" class="nav__logo">room</a>
+        <nav class="nav">
           <ul class="nav__items">
             <li class="nav__item" v-for="(navItem, index) in navItems" :key="index">
-              <a href="#" @click="navigateToSlide(index)">
+              <a href="#" @click="navigateToSlide(index)" class="nav__link">
                 {{ navItem }}
               </a>
-              <div class="nav__border link"></div>
+              <div class="nav__border"></div>
             </li>
           </ul>
         </nav>
       </div>
     </header>
+
+    <!-- Swiper Gallery -->
     <swiper
       ref="swiperRef"
       :modules="modules"
@@ -100,11 +102,12 @@ console.log('navOpen: ', navOpen)
       :autoplay="{ delay: 5000 }"
     >
       <swiper-slide v-for="image in images" :key="image">
-        <img :src="`./images/${image}`" alt="Gallery Image" />
+        <img :src="`./images/${image}`" alt="Gallery Image" class="gallery__image" />
       </swiper-slide>
     </swiper>
   </div>
 </template>
+
 <style scoped lang="scss">
 .gallery {
   background-image: url('../assets/images/desktop-image-hero-1.jpg');
@@ -117,6 +120,7 @@ console.log('navOpen: ', navOpen)
     height: 400px;
   }
 }
+
 header {
   display: flex;
   align-items: baseline;
@@ -124,45 +128,11 @@ header {
   position: relative;
   z-index: 100;
 
-  .logo {
+  .nav__logo {
     font-size: xx-large;
     margin-right: 1rem;
     color: white;
   }
-}
-a {
-  text-decoration: none;
-}
-.nav {
-  &__items {
-    display: flex;
-    gap: 1.4rem;
-    list-style: none;
-  }
-
-  &__item {
-    position: relative;
-    display: flex;
-    justify-content: center;
-    color: white;
-
-    // Add hover on .nav__item to target .nav__border
-    &:hover .nav__border {
-      border-bottom: 2px solid white;
-    }
-  }
-
-  &__border {
-    position: absolute;
-    top: 0;
-    width: 70%;
-    height: calc(100% + 5px);
-  }
-}
-img {
-  height: 100%;
-  width: 100%;
-  object-fit: cover;
 }
 
 .swiper {
@@ -170,28 +140,57 @@ img {
   inset: 0;
 }
 
-///////////////////////////
-
-#sidemenu {
+#desktop-menu {
   .nav {
-    // height: calc(100% - #{$headerHeight} - #{$footerHeight});
+    &__items {
+      display: flex;
+      gap: 1.4rem;
+      list-style: none;
+    }
+
+    &__item {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      color: white;
+
+      &:hover .nav__border {
+        border-bottom: 2px solid white;
+      }
+
+      .nav__link {
+        text-decoration: none;
+      }
+    }
+
+    &__border {
+      position: absolute;
+      top: 0;
+      width: 70%;
+      height: calc(100% + 5px);
+    }
+  }
+}
+
+// Mobile Menu Styles
+#mobile-menu {
+  .nav {
     background: white;
     position: fixed;
     top: 0;
     left: 0;
     right: 0;
     z-index: 99;
-    // box-shadow: 2px 0 3px$grey-6;
-    // overflow-y: scroll;
-  }
-  .sidemenu {
+
     &__wrapper {
       background-color: white;
+      padding: 50px;
+      padding-left: 0;
+      display: flex;
+      justify-content: flex-end;
     }
 
-    // @include show-on-mobile;
-
-    &__btn {
+    &__menu-btn {
       display: flex;
       width: 50px;
       height: 50px;
@@ -200,21 +199,12 @@ img {
       position: relative;
       z-index: 100;
       appearance: none;
-      position: relative;
       justify-content: center;
       align-items: center;
 
-      .hamburger,
-      .close {
+      .nav__icon {
         position: absolute;
       }
-    }
-
-    &__wrapper {
-      padding: 50px;
-      padding-left: 0;
-      display: flex;
-      justify-content: flex-end;
     }
 
     &__items {
@@ -222,31 +212,24 @@ img {
       gap: 1.4rem;
       list-style: none;
       padding-top: 50px;
-      list-style: none;
       padding: 0;
       margin: 0;
       width: fit-content;
-    }
 
-    &__item {
-      position: relative;
-      display: flex;
-      justify-content: center;
-    }
+      .nav__item {
+        .nav__link {
+          text-decoration: none;
+          line-height: 1.6em;
+          font-size: 1rem;
+          padding: 0.5em;
+          display: block;
+          color: black;
+          transition: 0.4s ease;
 
-    &__item {
-      a {
-        text-decoration: none;
-        line-height: 1.6em;
-        font-size: 1rem;
-        padding: 0.5em;
-        display: block;
-        color: black;
-        transition: 0.4s ease;
-
-        &:hover {
-          background: lightgrey;
-          color: dimgrey;
+          &:hover {
+            background: lightgrey;
+            color: dimgrey;
+          }
         }
       }
     }
@@ -254,7 +237,6 @@ img {
 }
 
 // Hamburger to Close Icon Animation
-
 .scale-enter-active,
 .scale-leave-active {
   transition: transform 0.2s ease;
